1ff6bf0b2fd8f1229fc20cd157d30ab8c1624b8d
tsarkoff <tsarkov.vladimir@gmail.com>
2024-06-22 02:51:36 +0300

    Dependency Injection via Annotations added (using MainServlet.AnnotationConfigApplicationContext)

--- src/main/java/ru/netology/servlet/MainServlet.java
+++ src/main/java/ru/netology/servlet/MainServlet.java
@@ -3,9 +3,10 @@ package ru.netology.servlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import ru.netology.controller.PostController;
-import ru.netology.repository.PostRepository;
-import ru.netology.service.PostService;
+//import ru.netology.repository.PostRepositoryStubImpl;
+//import ru.netology.service.PostService;
 
 public class MainServlet extends HttpServlet {
     private static final String METHOD_GET = "GET";
@@ -15,9 +16,11 @@ public class MainServlet extends HttpServlet {
 
     @Override
     public void init() {
-        final var repository = new PostRepository();
-        final var service = new PostService(repository);
-        controller = new PostController(service);
+//        final var repository = new PostRepositoryStubImpl();
+//        final var service = new PostService(repository);
+//        controller = new PostController(service);
+        final var context = new AnnotationConfigApplicationContext("ru.netology");
+        controller = context.getBean(PostController.class);
     }
 
     @Override
